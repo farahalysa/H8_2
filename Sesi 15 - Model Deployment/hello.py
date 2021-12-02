352bbf4970ab538b3df308ffba37c52d27645350
farahalysa <alysafarah@gmail.com>
2021-12-03 00:21:01 +0700

    add Sesi 15

---
+++ Sesi 15 - Model Deployment/hello.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask import request
+app = Flask(__name__)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return do_the_login()
+    else:
+        return show_the_login_form()
+
+@app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route('/hello')
+def hello_world():
+    return 'Hello, World!'
